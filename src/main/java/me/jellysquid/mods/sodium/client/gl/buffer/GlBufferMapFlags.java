072b5035f44c09c6d9f6bdc6165c3381db91d557
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-28 10:38:25 -0300

    Update GlBufferMapFlags.java

--- src/main/java/me/jellysquid/mods/sodium/client/gl/buffer/GlBufferMapFlags.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/buffer/GlBufferMapFlags.java
@@ -1,19 +1,28 @@
 package me.jellysquid.mods.sodium.client.gl.buffer;
 
 import me.jellysquid.mods.sodium.client.gl.util.EnumBit;
-import org.lwjgl.opengl.GL30C;
-import org.lwjgl.opengl.GL33C;
-import org.lwjgl.opengl.GL44C;
+import org.lwjgl.opengl.GL11;
 
+/**
+ * This enum cannot be ported to OpenGL 2.1 because it relies on features introduced in later versions.
+ *
+ * OpenGL 2.1 does not support the concept of mapping buffers for direct access.
+ *
+ * Therefore, the functionality of this enum is not available in OpenGL 2.1.
+ *
+ * @see <a href="https://www.khronos.org/registry/OpenGL-Refpages/es3.0/html/glMapBuffer.xhtml">glMapBuffer</a>
+ */
 public enum GlBufferMapFlags implements EnumBit {
-    READ(GL30C.GL_MAP_READ_BIT),
-    WRITE(GL30C.GL_MAP_WRITE_BIT),
-    PERSISTENT(GL44C.GL_MAP_PERSISTENT_BIT),
-    INVALIDATE_BUFFER(GL30C.GL_MAP_INVALIDATE_BUFFER_BIT),
-    INVALIDATE_RANGE(GL30C.GL_MAP_INVALIDATE_RANGE_BIT),
-    EXPLICIT_FLUSH(GL30C.GL_MAP_FLUSH_EXPLICIT_BIT),
-    COHERENT(GL44C.GL_MAP_COHERENT_BIT),
-    UNSYNCHRONIZED(GL33C.GL_MAP_UNSYNCHRONIZED_BIT);
+    // READ(GL30C.GL_MAP_READ_BIT),
+    // WRITE(GL30C.GL_MAP_WRITE_BIT),
+    // PERSISTENT(GL44C.GL_MAP_PERSISTENT_BIT),
+    // INVALIDATE_BUFFER(GL30C.GL_MAP_INVALIDATE_BUFFER_BIT),
+    // INVALIDATE_RANGE(GL30C.GL_MAP_INVALIDATE_RANGE_BIT),
+    // EXPLICIT_FLUSH(GL30C.GL_MAP_FLUSH_EXPLICIT_BIT),
+    // COHERENT(GL44C.GL_MAP_COHERENT_BIT),
+    // UNSYNCHRONIZED(GL33C.GL_MAP_UNSYNCHRONIZED_BIT);
+
+    // No support for buffer mapping in OpenGL 2.1
 
     private final int bit;
 
