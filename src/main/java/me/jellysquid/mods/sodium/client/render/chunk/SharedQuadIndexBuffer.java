08d21ba9941f5556750157eea7a2d28b3cc83ed6
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-27 21:58:46 -0300

    Update SharedQuadIndexBuffer.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/SharedQuadIndexBuffer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/SharedQuadIndexBuffer.java
@@ -26,6 +26,13 @@ public class SharedQuadIndexBuffer {
         this.indexType = indexType;
         this.maxPrimitives = indexType.getMaxPrimitiveCount(); // Set the maximum primitive count based on the chosen index type
 
+        public void ensureCapacity(CommandList commandList, int elementCount) {
+        if (elementCount > this.indexType.getMaxElementCount()) {
+            throw new IllegalArgumentException("Tried to reserve storage for more vertices in this buffer than it can hold");
+        }
+
+        int primitiveCount = elementCount / ELEMENTS_PER_PRIMITIVE;
+            
         // Create the buffer with the maximum capacity
         this.buffer = commandList.createMutableBuffer();
         allocateStorage(commandList);
